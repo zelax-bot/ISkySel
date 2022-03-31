634db12397b545edd81fbaa942ef578bdca2748c
zjh <1910644713@qq.com>
2022-03-31 11:04:25 +0800

    v1.0

--- src/demo.java
+++ src/demo.java
@@ -12,21 +12,32 @@ public class demo {
     public static void main(String[] args) {
         Evaluator ev = new Evaluator();
 
-        double[][] dataOrigin = FileHandler.readData("file/qws_normal.txt", false);
-        double[][] dataSparse = FileHandler.readData("file/m_rate_0.1_time_0.txt", false);
+        double[][] dataOrigin = FileHandler.readData("data/qws_normal.txt", false);
+        double[][] dataSparse = FileHandler.readData("data/m_rate_0.1_time_0.txt", false);
 
         List<double[][]> dataImpute = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            dataImpute.add(FileHandler.readData("file/qws_m_rate_0.1_time_0_prt_" + i + ".txt", false));
+            dataImpute.add(FileHandler.readData("data/qws_m_rate_0.1_time_0_prt_" + i + ".txt", false));
         }
         double[][][] dataInterval = DataHandler.buildIntervalData(dataImpute);
 
         int[] skyline = new CompleteSkyline().sortedFilterSkyline(dataOrigin);
 
-        long start = System.currentTimeMillis();
-        int[] skylinePredict = new ISkySel().getSkyline(dataSparse, dataInterval, 20, 50);
-        long end = System.currentTimeMillis();
+        int bins = 20, topk = 50;
 
+        long start = System.currentTimeMillis();
+        int[] skylinePredictNative = new ISkySel().getSkylineNative(dataSparse, dataInterval, bins, topk);
+        long end = System.currentTimeMillis();
+        System.out.println("cost: " + (end - start) / 1000.0 + "s\t pre: " + ev.precision(skyline, skylinePredictNative));
+
+        start = System.currentTimeMillis();
+        int[] skylinePredictThreshold = new ISkySel().getSkylineNativeWithThreshold(dataSparse, dataInterval, bins, topk);
+        end = System.currentTimeMillis();
+        System.out.println("cost: " + (end - start) / 1000.0 + "s\t pre: " + ev.precision(skyline, skylinePredictThreshold));
+
+        start = System.currentTimeMillis();
+        int[] skylinePredict = new ISkySel().getSkyline(dataSparse, dataInterval, bins, topk);
+        end = System.currentTimeMillis();
         System.out.println("cost: " + (end - start) / 1000.0 + "s\t pre: " + ev.precision(skyline, skylinePredict));
     }
 }
